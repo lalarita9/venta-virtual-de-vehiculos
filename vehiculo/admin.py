f72a125a500d1faa59ad9e1f9033dc5e8be9bc5e
Carolina Lara <lalarita@hotmail.com>
2023-07-02 19:08:56 -0400

    Registro app Vehiculo a sitio admin Django

--- vehiculo/admin.py
+++ vehiculo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import VehiculoModel
+
+#Registro de App Vehiculo a sitio Django(Drilling Final, Parte 1)
+admin.site.register(VehiculoModel)
